db152cc7ee7ffa4d47ec0d30f185b3d07e04aee0
MuyeedAhmed <akib100095@gmail.com>
2024-05-27 20:09:55 -0400

    Rename slideshow to Stock

--- app/src/main/java/com/max/z/ui/stock/StockFragment.kt
+++ app/src/main/java/com/max/z/ui/stock/StockFragment.kt
@@ -1,4 +1,4 @@
-package com.max.z.ui.slideshow
+package com.max.z.ui.stock
 
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -7,11 +7,11 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
-import com.max.z.databinding.FragmentSlideshowBinding
+import com.max.z.databinding.FragmentStockBinding
 
-class SlideshowFragment : Fragment() {
+class StockFragment : Fragment() {
 
-    private var _binding: FragmentSlideshowBinding? = null
+    private var _binding: FragmentStockBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
@@ -22,14 +22,14 @@ class SlideshowFragment : Fragment() {
             container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
-        val slideshowViewModel =
-                ViewModelProvider(this).get(SlideshowViewModel::class.java)
+        val stockViewModel =
+                ViewModelProvider(this).get(StockViewModel::class.java)
 
-        _binding = FragmentSlideshowBinding.inflate(inflater, container, false)
+        _binding = FragmentStockBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textSlideshow
-        slideshowViewModel.text.observe(viewLifecycleOwner) {
+        val textView: TextView = binding.textStock
+        stockViewModel.text.observe(viewLifecycleOwner) {
             textView.text = it
         }
         return root
--- app/src/main/java/com/max/z/ui/stock/StockViewModel.kt
+++ app/src/main/java/com/max/z/ui/stock/StockViewModel.kt
@@ -1,10 +1,10 @@
-package com.max.z.ui.slideshow
+package com.max.z.ui.stock
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class SlideshowViewModel : ViewModel() {
+class StockViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
